51ae19d615aedccb20c2d715a505dd0efb4dd72c
Nadi <nadinka121@rambler.ru>
2023-12-22 10:16:49 +0300

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+
+
+public class AppTest
+{
+    // Получить список всех сущностей
+    @Test
+    public void getAllMethods ()
+    { Response response;
+       response = RestAssured
+                .given().log().all()
+                .get("https://swapi.dev/api/");
+                response
+                       .then()
+                        //.log().body()
+                        .statusCode(200);
+        //String responseBody= response.getBody().asString();
+        //System.out.println(responseBody);
+        JsonPath jsonPath= response.jsonPath();
+        System.out.println(jsonPath.getString("$"));
+    }
+    // Получить список фильмов (films) и информацию по одному фильму
+    @Test
+    public void getFilmInfo ()
+    { Response response;
+        response = RestAssured
+                .given().log().all()
+                .get("https://swapi.dev/api/films");
+        response.then().statusCode(200);
+
+    }
+
+
+}
