6241dd215e11760c1e07763b42262d29af49093c
Nadi <nadinka121@rambler.ru>
2023-12-27 13:29:09 +0300

    refactoring

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,75 +1,85 @@
 package org.example;
 
+
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
-import java.util.List;
 import java.util.Map;
 
 
 public class AppTest
-{
+{  String baseURL = "https://swapi.dev/api/";
     // Получить список всех сущностей
     @Test
-    public void getAllMethods ()
-    { Response response;
+    public void getAllMethods () {
+        Response response;
         Map<String,String> result;
-       response = getResponse("https://swapi.dev/api/");
-       checkResponse(response);
-        //String responseBody= response.getBody().asString();
-        //System.out.println(responseBody);
+        response = getResponse(baseURL);
+        checkResponse(response);
         JsonPath jsonPath= response.jsonPath();
         result = jsonPath.getMap("$");
-        for (String keys: result.keySet()){
-            System.out.println(keys);
+        for (Map.Entry element: result.entrySet()) {
+            System.out.println(element);
         }
     }
-    // Получить список фильмов (films) и информацию по одному фильму
+    // Получить список фильмов (films)
+
     @Test
-    public void getFilmInfo ()
-    { Response response;
-        Map<String,String> result;
-        response = getResponse("https://swapi.dev/api/films");
+    public void getFilmList () {
+        Response response;
+        response = getResponse(baseURL + "/films");
         checkResponse(response);
         JsonPath jsonPath= response.jsonPath();
         System.out.println("\n\n"+ jsonPath.getList("results.title") + "\n\n");
-        result = jsonPath.getMap("results[3]");
+    }
+
+    // Получить инфо по фильму 6
+    @Test
+    public void getFilmInfo () {
+        Response response;
+        Map<String,String> result;
+        response = getResponse(baseURL + "/films/6");
+        checkResponse(response);
+        JsonPath jsonPath= response.jsonPath();
+        result = jsonPath.getMap("$");
         for (Map.Entry element: result.entrySet()) {
             System.out.println(element);
         }
     }
 
-   //Получить список планет (planets) и информацию по планете выбранного вам фильма
+   //Получить список планет (planets)
     @Test
-    public void getPlanetInfo () {
-        Response responseFilms;
-        Response responsePlanet;
-        List<String> planet;
+    public void getPlanetList () {
+        Response response;
+        response = getResponse(baseURL + "/planets");
+        checkResponse(response);
+        JsonPath jsonPath= response.jsonPath();
+        System.out.println("\n\n"+ jsonPath.getList("results.name") + "\n\n");
+    }
+
+    //Получить инфо по планете 3 из фильма 1
+    @Test
+    public void getPlanetInfo() {
+        Response response;
         Map<String,String> result;
-        responseFilms = getResponse("https://swapi.dev/api/films");
-        checkResponse(responseFilms);
-        JsonPath jsonPath= responseFilms.jsonPath();
-        planet = jsonPath.getList("results[3].planets");
-        responsePlanet = getResponse(planet.get(0));
-        checkResponse(responsePlanet);
-        JsonPath jsonPathPlanet= responsePlanet.jsonPath();
-        System.out.println(planet + "\n");
-        result = jsonPathPlanet.getMap("");
+        response = getResponse(baseURL + "/planets/3");
+        checkResponse(response);
+        JsonPath jsonPath= response.jsonPath();
+        result = jsonPath.getMap("");
         for (Map.Entry element: result.entrySet()) {
             System.out.println(element);
-        }
+       }
     }
     public Response getResponse(String url) {
         return  RestAssured
                 .given().log().all()
                 .get(url);
     }
-    void checkResponse (Response response) {
+    public void checkResponse (Response response) {
         response
                 .then()
                 //.log().body()
                 .statusCode(200);
     }
-
 }
